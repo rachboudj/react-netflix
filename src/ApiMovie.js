860606521d972bd09f8a60062e5833023c760fd3
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-16 22:27:41 +0200

    ajout de différentes catégories à afficher dans la fonction

--- src/ApiMovie.js
+++ src/ApiMovie.js
@@ -1,10 +1,31 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = "https://api.themoviedb.org/3/";
 
+const fetchMovies = async (endpoint) => {
+    return await fetch(
+        `${API_URL}{endpoint}&language=fr-FR&api_key=${API_KEY}`
+    ).then((response) => response.json())
+    
+}
+
 export default {
     getHomeMovies: async () => {
         return [
-            
+            {
+                slug: "top-rated",
+                title: "Mieux notés",
+                items: await fetchMovies("movie/top_rated"),
+            },
+            {
+                slug: "upcoming",
+                title: "Prochaines sorties ",
+                items: await fetchMovies("movie/upcoming"),
+            },
+            {
+                slug: "action",
+                title: "Action",
+                items: await fetchMovies("movie?with_genres=28"),
+            }
         ]
     }
 }

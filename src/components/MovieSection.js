ce27c6314d7a8a68602f6d880ce92cd22a4ef1de
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-17 23:53:41 +0200

    affichage des affiches de film et ajout d'une condition au cas où l'image n'est pas présente

--- src/components/MovieSection.js
+++ src/components/MovieSection.js
@@ -3,9 +3,21 @@ import "./MovieSection.css";
 
 function MovieSection({title, items}) {
     return (
-        <div>
+        <div className='movieRow'>
             <h2>{title}</h2>
-            <div className='movieRow--listarea'></div>
+            <div className='movieRow--listarea'>
+                <div className="movieRow--list">
+                    {
+                        items.results.length > 0 && items.results.map((item, key) => (
+                            <div className="movieRow--item" key={key}>
+                                <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.original_title} />
+                            </div>
+                            
+                        ))
+                    }
+                </div>
+                
+            </div>
         </div>
     );
 }

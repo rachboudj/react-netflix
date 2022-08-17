e31ba035fd70c7925945c8fb1863e23dcb8df4f9
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-17 22:49:36 +0200

    affichage du titre de la catégorie des films

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ApiMovie from "./ApiMovie";
 import './App.css';
 
 function App() {
+  const [movieList, setMoviesList] = useState([])
+
   useEffect(() => {
       const loadAllMovies = async () => {
-        let moviesList = await ApiMovie.getHomeMovies()
+        let list = await ApiMovie.getHomeMovies()
 
-        console.log(moviesList)
+        console.log(list)
+        setMoviesList(list)
       }
       loadAllMovies()
   }, [])
 
   return (
-    <div className="App">Hello World</div>
+    <div className="container">
+      <section className="list">
+        {
+          movieList.map((item, key) => (
+            <div>{item.title}</div>
+          ))
+        }
+      </section>
+    </div>
   );
 }
 

0f378418ab2595844a0051fd9420f738d95ddfb5
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-17 22:57:41 +0200

    ajout de la page qui affichera tous les films et catégories

---
+++ src/components/MovieSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MovieSection.css";
+
+function MovieSection() {
+    return (
+        <div>
+            MovieSection
+        </div>
+    );
+}
+
+export default MovieSection;
